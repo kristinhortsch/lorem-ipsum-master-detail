98953f2cb58cd45ee1cd0cc42c093497830cdb9c
Kristin Hortsch <kristinhortsch@gmail.com>
2018-11-28 13:38:29 -0800

    fixed eslint and made header component which successfully links to app and renders to page

--- data.js
+++ data.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 export default [
     {
         title:       'Bob Ross Ipsum',
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <h1>Lorem Ipsum List</h1>
+        <img src="../../public/lorem-logo.png">
+    </header>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+    header {
+        background: black;
+        color: white;
+        text-align: center;
+        margin-top: 0px;
+        border: 10px solid lightgray;
+    }
+
+    img {
+        width: 100%;
+        max-width: 300px;
+        border-radius: 5px;
+        border: 5px solid gray;
+        margin-bottom: 20px;
+    }
+
+    h1 {
+        margin-top: 0px;
+        margin-bottom: 5px;
+        padding: 10px;
+        font-size: 4em;
+    }
+
+</style>
